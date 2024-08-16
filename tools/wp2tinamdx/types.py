137ec43859e86bfe3cb757a0aae87d3e6ee6668a
Takashi Ando <dodo5522@gmail.com>
2024-08-16 21:58:52 +0900

    [tools] Make converter to add tags and description

--- tools/wp2tinamdx/types.py
+++ tools/wp2tinamdx/types.py
@@ -7,4 +7,5 @@ class Args:
     output_dir: str  # Markdown出力先ディレクトリへのパス文字列
     set_author: str  # Markdownファイルの著者文字列
     map_category: str  # Category文字列変換マップファイル
+    published_only: bool  # 公開済みのもののみ対象とする
     debug: bool  # デバッグ用
