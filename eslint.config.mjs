4484b9e2c173cfcfc749276b5382c81b918fb6a5
Takashi Ando <dodo5522@gmail.com>
2026-04-20 00:25:12 +0900

    Set up Astro toolchain

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import astro from "eslint-plugin-astro";
+import tseslint from "typescript-eslint";
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,astro}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: ["dist/", ".astro/", "node_modules/"],
+  },
+];
