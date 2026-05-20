76d0471809a9f399acc832951f217d882bb5317e
Takashi Ando <dodo5522@gmail.com>
2026-05-20 10:10:10 +0900

    Add Terraform for site and Studio hosting

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,33 @@
-output "bucket_name" {
-  value = aws_s3_bucket.site.bucket
+output "site_bucket_name" {
+  value = aws_s3_bucket.static["site"].bucket
 }
 
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.site.id
+output "site_cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.static["site"].id
+}
+
+output "site_cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.static["site"].domain_name
+}
+
+output "site_url" {
+  value = "https://${aws_cloudfront_distribution.static["site"].domain_name}"
+}
+
+output "studio_bucket_name" {
+  value = aws_s3_bucket.static["studio"].bucket
+}
+
+output "studio_cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.static["studio"].id
+}
+
+output "studio_cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.static["studio"].domain_name
+}
+
+output "studio_url" {
+  value = "https://${aws_cloudfront_distribution.static["studio"].domain_name}"
 }
 
 output "github_actions_deploy_role_arn" {
